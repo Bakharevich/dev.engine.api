34eb69b57f52bfb7f1fe52981f590b204d93d75b
Bakharevich <ilya@bakharevich.by>
2016-09-30 11:35:56 +0300

    Added search by options in category

--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -9,22 +9,30 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Category;
 use App\Company;
+use Input;
 
 class CategoryController extends Controller
 {
     public function show(Request $request, $city, $domain)
     {
+        $selectedOptions = $request->input('option', []);
+        
         // get category with options
         $category = Category::with('options_groups.options')->where('domain', $domain)->where('site_id', $request->site->id)->first();
 
         // get company
-        $companies = Company::where('category_id', $category->id)->paginate(1);
+        $companies = Company::whereHas('options', function($query) use ($selectedOptions) {
+            // if have options, get companies with them
+            if (!empty($selectedOptions)) $query->whereIn('option_id', $selectedOptions);
+        })->where('category_id', $category->id)->paginate(1);
+
         // get options
 
 
         return view('category', [
             'category' => $category,
-            'companies' => $companies
+            'companies' => $companies,
+            'selectedOptions' => $selectedOptions
         ]);
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -8,40 +8,68 @@
             <li class="active">{{ $category->name }}</li>
         </ol>
 
-        @if ($category->description_top && Request::get('page') == 1)
-            <p>{{ $category->description_top }}</p>
-        @endif
+        <div class="col-md-2">
+            @if ($category->options_groups)
+                <form action="" method="GET" id="form_options">
+                @foreach ($category->options_groups as $options_group)
+                    <b>{{ $options_group->name }}</b><br/>
 
-        @foreach ($category->options_groups as $options_group)
-            {{ $options_group->name }}<br/><br/>
-
-            @foreach ($options_group->options as $option)
-                {{ $option->name }}<br/>
-            @endforeach
-        @endforeach
-
-        @if (count($companies) > 0)
-            <div class="row">
-                @foreach ($companies as $company)
-                    <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6 text-center">
-                        <a href="http://{{ $company->domain }}.{{ \Request::get('site')->domain }}/">
-                            <div style="border: 1px solid #CCC; height: 175px;">
-                            {{ $company->name }}
-                            </div>
-                        </a>
+                    @foreach ($options_group->options as $option)
+                    <div class="checkbox">
+                        <label>
+                            @if (!empty($selectedOptions[$option['id']]))
+                                <input name="option[{{ $option->id }}]" value="{{ $option->id }}" type="checkbox" checked> {{ $option->name }}
+                            @else
+                                <input name="option[{{ $option->id }}]" value="{{ $option->id }}" type="checkbox"> {{ $option->name }}
+                            @endif
+                        </label>
                     </div>
+                    @endforeach
                 @endforeach
-            </div>
+                </form>
+            @endif
 
-            {{ $companies->links() }}
-        @else
-            <p>
-                Нет компаний в данной категории.
-            </p>
-        @endif
+        </div>
+        <div class="col-md-10">
+            @if ($category->description_top && Request::get('page') == 1)
+                <p>{{ $category->description_top }}</p>
+            @endif
 
-        @if ($category->description_top && Request::get('page') == 1)
-            <p>{{ $category->description_bottom }}</p>
-        @endif
+            @if (count($companies) > 0)
+                <div class="row">
+                    @foreach ($companies as $company)
+                        <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6 text-center">
+                            <a href="http://{{ $company->domain }}.{{ \Request::get('site')->domain }}/">
+                                <div style="border: 1px solid #CCC; height: 175px;">
+                                    {{ $company->name }}
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+
+                {{ $companies->appends(['option' => $selectedOptions])->links() }}
+            @else
+                <p>
+                    Нет компаний в данной категории.
+                </p>
+            @endif
+
+            @if ($category->description_top && Request::get('page') == 1)
+                <p>{{ $category->description_bottom }}</p>
+            @endif
+        </div>
     </div>
+@stop
+
+@section('scripts')
+    <script>
+        $(function(){
+            $(':checkbox').change(function(){
+                var ser = $("#form_options").serialize();
+
+                document.location = '/{{ Request::get('site')->city->domain }}/{{ $category->domain }}/?' + ser;
+            })
+        });
+    </script>
 @stop
