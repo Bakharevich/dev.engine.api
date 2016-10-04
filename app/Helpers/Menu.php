38a2245bee234a58c4ad6fb97728440389211fdd
Bakharevich <ilya@bakharevich.by>
2016-10-04 15:32:16 +0300

    Added custom Helper to Laravel

---
+++ app/Helpers/Menu.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Helpers;
+
+class Menu {
+    public static function get($type = '')
+    {
+        //return $type;
+        //return self::getSimpleMenu();
+    }
+
+    public function getSimpleMenu()
+    {
+
+    }
+
+    public function getBigMenu()
+    {
+
+    }
+}
