8689c6b1663cb018032c675b88b45d0e08a9b254
Bakharevich <ilya@bakharevich.by>
2016-10-04 09:49:47 +0300

    Fixed searching companies by options

--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -10,24 +10,32 @@ use App\Http\Requests;
 use App\Category;
 use App\Company;
 use Input;
+use DB;
 
 class CategoryController extends Controller
 {
     public function show(Request $request, $city, $domain)
     {
         $selectedOptions = $request->input('option', []);
-        
+
+        DB::enableQueryLog();
+
         // get category with options
         $category = Category::with('options_groups.options')->where('domain', $domain)->where('site_id', $request->site->id)->first();
 
-        // get company
-        $companies = Company::whereHas('options', function($query) use ($selectedOptions) {
-            // if have options, get companies with them
-            if (!empty($selectedOptions)) $query->whereIn('option_id', $selectedOptions);
-        })->where('category_id', $category->id)->paginate(1);
+        // if there are selected companies, need different query
+        if (!empty($selectedOptions)) {
+            $companies = Company::whereHas('options', function ($query) use ($selectedOptions) {
+                // if have options, get companies with them
+                if (!empty($selectedOptions)) $query->whereIn('option_id', $selectedOptions);
+            })->where('category_id', $category->id)->paginate(1);
+        }
+        else {
+            $companies = Company::with('options')->where('category_id', $category->id)->paginate(1);
+        }
 
         // get options
-
+        //echo "<pre>";print_r(DB::getQueryLog()); echo "</pre>";
 
         return view('category', [
             'category' => $category,
