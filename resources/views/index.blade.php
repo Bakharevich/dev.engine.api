46b7796078641a020846dffe2afa58919949730b
Bakharevich <ilya@bakharevich.by>
2016-10-11 10:12:33 +0300

    Update index template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,42 @@
             padding-top: 50px;
             background: #f0f1f2;
         }
+        A {
+            /*color: #00928c;*/
+        }
+        .index-link-category:hover {
+            text-decoration: none;
+        }
+        .navbar-default {
+            background: #f53118;
+
+        }
+        .navbar-default A {
+            color: #FFF !important;
+        }
+        .blur {
+            -webkit-filter: blur(10px); filter: blur(10px);
+        }
+        .index-bg {
+            background: linear-gradient(
+                to bottom,
+                rgba(0, 0, 0, 0),
+                rgba(0, 0, 0, 0.6)
+        ), url({{ Request::get('site')->media_url }}/backgrounds/index.jpg);
+
+            background-size: cover;
+            background-position: left top !important;
+            /*-webkit-filter: blur(5px);  -moz-filter: blur(5px);  -o-filter: blur(5px);  -ms-filter: blur(5px);  filter: blur(5px);*/
+            padding: 195px 0px 225px 0px;
+            margin-bottom: 30px;
+        }
+        .index-bg {
+
+
+        }
     </style>
 
-    <div class="text-center" style="margin-bottom: 30px; padding: 195px 0px 225px 0px; background-image: url({{ Request::get('site')->media_url }}/backgrounds/index.jpg); background-size: cover; background-position: left top !important;">
+    <div class="index-bg text-center">
         <h1 style="color: #FFF; font-size: 4em; margin-bottom: 35px; text-shadow: 1px 1px #000;">{{ Request::get('site')->headline }}</h1>
 
         <div style="max-width: 800px; margin: 0 auto; padding: 0px 30px 0px 30px;">
@@ -21,13 +54,13 @@
         </div>
     </div>
 
-    <div class="container" style="margin: 0 auto;">
+    <div class="container" style="margin: 0 auto; margin-bottom: 30px;">
         @if (count($categories) > 0)
             <div class="row">
                 @foreach ($categories as $category)
                     <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6 text-center">
-                        <a href="/{{ Request::get('site')->city->domain }}/{{ $category->domain }}/">
-                            <div style="border: 1px solid #CCC; padding: 35px 0px 35px 0px; background: #FFF; ">
+                        <a href="/{{ Request::get('site')->city->domain }}/{{ $category->domain }}/" class="index-link-category">
+                            <div style="border: 1px solid #CCC; padding: 35px 0px 35px 0px; background: #FFF; border-radius: 5px; box-shadow: 4px; box-shadow: 0 1px 3px rgba(48, 53, 64, .3);">
                                 <div>
                                     <i class="fa {{ $category->icon }}" aria-hidden="true" style="font-size: 64px;"></i>
                                 </div>
