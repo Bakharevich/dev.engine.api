0741eda157de2ae2853fdc71a11c46797f60fbfa
Bakharevich <ilya@bakharevich.by>
2016-10-11 20:28:13 +0300

    Removed limiting in scraper till 1 company

---
+++ app/Jobs/Scraper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Scraper implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // get list of scraped settings
+        echo "Working...<br/>\n\n";
+
+        // process every pages
+    }
+}
