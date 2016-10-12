103adfd6a0746f6e09332d757f842f22f7d7289e
Bakharevich <ilya@bakharevich.by>
2016-10-12 23:50:32 +0300

    Adding meta to main page

--- app/Http/Controllers/Web/IndexController.php
+++ app/Http/Controllers/Web/IndexController.php
@@ -9,16 +9,22 @@ use App\Http\Requests;
 use Cache;
 use App\Category;
 use Route;
+use App\Meta;
 
 
 class IndexController extends Controller
 {
     public function index(Request $request)
     {
+        // get categories
         $categories = Category::where('site_id', $request->site->id)->get();
 
+        // get meta
+        $meta = Meta::where('site_id', $request->site->id)->where('url', $_SERVER['REQUEST_URI'])->first();
+
         return view('index', [
-            'categories' => $categories
+            'categories' => $categories,
+            'meta' => $meta
         ]);
     }
 }
