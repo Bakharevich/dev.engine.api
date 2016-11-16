100fff77aa0fd16152b3deef6a4571c9c848d512
Bakharevich <ilya@bakharevich.by>
2016-11-16 22:11:01 +0300

    Update Yelp settings

--- app/Scrapers/Yelp.php
+++ app/Scrapers/Yelp.php
@@ -23,8 +23,8 @@ class Yelp extends Scraper implements ScraperInterface {
     public function __construct()
     {
         //$this->companyRepo = $companyRepo;
-        $this->limitPhotos = 1;
-        $this->limitCompanies = 1; // 0 means unlimited
+        $this->limitPhotos = 20;
+        $this->limitCompanies = 0; // 0 means unlimited
     }
 
     public function process($url)
