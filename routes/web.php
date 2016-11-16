5eb9694b0302f207487349f178799e4dea3fd6e1
Bakharevich <ilya@bakharevich.by>
2016-11-16 23:02:18 +0300

    Update list of domains

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Repositories\CompanyRepository;
 //use GuzzleHttp;
 
 $domains = [
-    'yelpster.dev', 'yelpster.net', 'ibelarus.dev', 'ibelarus.by'
+    'yelpster.dev', 'yelpster.net', 'ibelarus.dev', 'ibelarus.by', 'infomalaysia.net', '24india.in'
 ];
 
 foreach ($domains as $domain) {
@@ -17,9 +17,51 @@ Route::group(['domain' => '{companyDomain}.ibelarus.by'], function() {
     Route::get('/', 'Web\CompanyController@show');
 });
 
+Route::get('/categoriesstats/', function() {
+    $category = 'https://www.yelp.com/search?find_loc=Kuala+Lumpur&cflt=shopping';
+
+    $categoryPage = file_get_contents($category);
+
+    $reg = "#<a class='category-browse-anchor js-search-header-link' href=\"(.+?)\">(.+?)</a>#";
+    preg_match_all($reg, $categoryPage, $matches);
+
+    //echo "<pre>"; print_r($matches);
+
+    $result = [];
+
+    if (!empty($matches)) {
+        $i = 1;
+        foreach ($matches[1] as $index => $value) {
+            if ($i < 49) {
+                $i++;
+                continue;
+            }
+            $url = 'https://www.yelp.com' . $matches[1][$index];
+            $name = $matches[2][$index];
+
+            $page = file_get_contents($url);
+
+            $reg = "|<span class=\"pagination-results-window\">.*?Showing.*?of ([0-9]{1,}).*?</span>|is";
+            preg_match_all($reg, $page, $match);
+            //echo "<pre>"; print_r($match);
+
+            if (!empty($match[1][0])) $result[$match[1][0]] = ['url' => $url, 'name' => $name];
+
+            if ($i == 70) break;
+            $i++;
+        }
+    }
+
+    krsort($result);
+
+    echo "<pre>"; print_r($result);
+});
+
 Route::get('/', 'Web\IndexController@index')->name('index');
 
 
 //Route::get('/{city}/{category}/{company}', 'CompanyController@show');
 Route::get('/{city}/{category}', 'Web\CategoryController@show');
 Route::get('/{city}/', 'Web\CityController@show');
+
+
