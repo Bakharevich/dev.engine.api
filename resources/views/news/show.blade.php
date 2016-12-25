073fb70360251132e386f024f6857491ceb40eb8
Bakharevich <ilya@bakharevich.by>
2016-12-25 17:48:25 +0300

    Added description to template

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div class="container" style="background-color: #fff; padding-bottom: 30px;">
         <h1>{{ $news->title }}</h1>
+        @if ($news->description)
+        <p><b>{{ $news->description }}</b></p>
+        @endif
         {!! $news->content  !!}
     </div>
 @stop
