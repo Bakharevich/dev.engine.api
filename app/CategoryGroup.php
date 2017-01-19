182e7dc64a422fb258fa5b00e887eeebd8b18cd3
Bakharevich <ilya@bakharevich.by>
2017-01-19 22:46:20 +0300

    Add fillable columns to CategoryGroup table

--- app/CategoryGroup.php
+++ app/CategoryGroup.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryGroup extends Model
 {
     protected $table = 'categories_groups';
+    protected $fillable = [
+        'site_id', 'city_id', 'name1', 'name2', 'created_at', 'updated_at'
+    ];
 
     public function categories()
     {
