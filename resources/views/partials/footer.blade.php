2b80cd29ac85c5ec92bf1f078f3120c541e7d464
Bakharevich <ilya@bakharevich.by>
2017-02-15 23:15:51 +0300

    Fix footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="site-footer">
-    <a href="/page/about">{{ trans('common.footer_about') }}</a> |
-    <a href="">{{ trans('common.footer_contact') }}</a> |
+    <a href="http://<?= Request::get('site')->domain ?>/page/about">{{ trans('common.footer_about') }}</a> |
+    <a href="mailto:ib@gogo.by">{{ trans('common.footer_contact') }}</a> |
     <a href="http://<?= Request::get('site')->domain ?>/companies/create">{{ trans('common.footer_add_business') }}</a> |
-    <a href="/page/vacancies">{{ trans('common.footer_vacancies') }}</a>
+    <a href="http://<?= Request::get('site')->domain ?>/page/vacancies">{{ trans('common.footer_vacancies') }}</a>
     <br/>
     {!! Request::get('site')->name !!}, {{ date("Y") }}
 </footer>
